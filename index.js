1f3ff4237c130aebce8df0dc9adb112033a972e8
kykp <kykp@yandex.ru>
2022-07-06 20:38:37 +0400

    heroku

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 const buildPath = path.join(__dirname, "client/build/index.html");
 app.use(express.static(buildPath));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client/build/index.html"));
 });
 
