8eed0ad608d0de97ee8cdcc7ac3a3767873d600e
kykp <kykp@yandex.ru>
2022-07-06 19:03:11 +0400

    added routing

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ import {
 import { formatMessage } from "./utils/message.js";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/public", "index.html"));
 });
 // const PORT = 3002;
 const port = process.env.PORT || 5000;
