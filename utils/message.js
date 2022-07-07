f588b55caf48f66f86fe7c913bee3c8c094bbf8c
kykp <kykp@yandex.ru>
2022-07-07 12:18:24 +0400

    fix: time fixed added 0, if minutes < 9

--- utils/message.js
+++ utils/message.js
@@ -1,5 +1,16 @@
-const time =
-  new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes();
+const hours = new Date(Date.now()).getHours();
+
+const minutes = () => {
+  const minutes = new Date(Date.now()).getMinutes();
+  if (minutes <= 9) {
+    let minute = `0 ${minutes}`;
+    return minute;
+  } else {
+    return minutes;
+  }
+};
+
+const time = `${hours} : ${minutes()}`;
 
 export function formatMessage(userName, message) {
   return {
